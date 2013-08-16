01542675335847f368c35cd2a403119e000c9a72
Pavel Dedik <dedikx@gmail.com>
2013-08-16 14:43:14 +0200

    Renamed the folder as wall.

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php if (!defined('APPLICATION')) exit();
+$ThemeInfo['IceFireBootstrap'] = array(
+   'Name' => 'IceFireBootstrap',
+   'Description' => 'Vanilla theme based on the Bootstrap framework by Twitter and VanillaBootstrap by Kasper Isager.',
+   'Version' => '2.1b-2.1',
+   'Author' => 'Pavel Dedik and Michal Ischia',
+   'AuthorEmail' => 'dedikx@gmail.com',
+   'AuthorUrl' => 'http://www.icefire.cz'
+);
