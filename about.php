a6ca0aaa6c3cbf3df483b9975fd2c85b09b505fe
Pavel Dedik <dedikx@gmail.com>
2015-07-23 20:07:43 +0200

    Authors.

--- about.php
+++ about.php
@@ -3,7 +3,7 @@ $ThemeInfo['IceFireBootstrap'] = array(
    'Name' => 'IceFireBootstrap',
    'Description' => 'Vanilla theme based on the Bootstrap framework by Twitter and VanillaBootstrap by Kasper Isager.',
    'Version' => '2.1b-2.1',
-   'Author' => 'paulcz, Alek and Montezuma3',
+   'Author' => 'Pavel Dedík, Michal Ischia and Tomáš Hodas',
    'AuthorEmail' => 'dedikx@gmail.com',
    'AuthorUrl' => 'http://www.icefire.cz'
 );
